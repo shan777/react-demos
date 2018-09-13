b4b29f114bdb70b6e435dc47e7ab21c87bb634cc
Sarah Han <sarah.s.han@gmail.com>
2018-09-12 18:40:09 -0700

    functional component challenge completed

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import Greeting from './greeting';
 
 const App = () => (
     <div>
         <div className="app">
             <img src={logo} className="logo rotate"/>
             <h1>Welcome to React</h1>
+            <Greeting name="Sarah" />
         </div>
     </div>
 );
